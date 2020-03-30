7315929faad5a43d38cc8d0cb92f6e810cafcc87
Xavier Lu <xll2@cornell.edu>
2020-03-30 18:18:28 -0400

    Updates

---
+++ precache-manifest.15e05902e33434855993ea64eda015a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e12b318d10d487bcfeed54bdd5d2ca8",
+    "url": "/blackjack-engine/index.html"
+  },
+  {
+    "revision": "662c09a9e18262a1441c",
+    "url": "/blackjack-engine/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "f7a718ee2d381dae7c34",
+    "url": "/blackjack-engine/static/js/2.bf11f069.chunk.js"
+  },
+  {
+    "revision": "662c09a9e18262a1441c",
+    "url": "/blackjack-engine/static/js/main.a9603195.chunk.js"
+  },
+  {
+    "revision": "1ad87e337746f0c82341",
+    "url": "/blackjack-engine/static/js/runtime~main.4213349d.js"
+  }
+]);
